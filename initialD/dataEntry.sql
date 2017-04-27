98e0d869f1d9ff5978584ee8f4b2528b6a259fc5
Laaden12345 <erkkipentti123@gmail.com>
2017-04-27 15:42:22 +0300

    Update dataEntry.sql

--- initialD/dataEntry.sql
+++ initialD/dataEntry.sql
@@ -2,6 +2,12 @@
 INSERT INTO Auto
 VALUES ('ABC-123')
 
+INSERT INTO Auto
+VALUES ('LAS-51')
+
+INSERT INTO Auto
+VALUES ('MOR-0')
+
 /* Henkilo */
 INSERT INTO Henkilo
 VALUES ('Lassi Knuuttila','0400-123123','Konemiehentie 2','123456-789A');
@@ -10,10 +16,42 @@ VALUES ('Lassi Knuuttila','0400-123123','Konemiehentie 2','123456-789A');
 
 
 /* Huoltopaketti */
+INSERT INTO Huoltopaketti
+VALUES ('Helahoito')
 
+INSERT INTO Huoltopaketti
+VALUES ('Vaan nää renkaat')
+
+INSERT INTO Huoltopaketti
+VALUES ('Nesteet uusiks')
+
+INSERT INTO Huoltopaketti
+VALUES ('Määräaikaishuolto')
 
 /* Laite */
+INSERT INTO Laite
+VALUES ('Ruuvari')
 
+INSERT INTO Laite
+VALUES ('Meisseli')
+
+INSERT INTO Laite
+VALUES ('Vasara')
+
+INSERT INTO Laite
+VALUES ('Tunkki')
+
+INSERT INTO Laite
+VALUES ('Lava kaliaa')
+
+INSERT INTO Laite
+VALUES ('Nosturi')
+
+INSERT INTO Laite
+VALUES ('Hitsausvälineet')
+
+INSERT INTO Laite
+VALUES ('Maalausvälineet')
 
 /* Laitetyyppi */
 
