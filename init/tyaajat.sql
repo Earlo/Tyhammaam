7e84a82505c94ea148887748664099950191beec
Laaden12345 <erkkipentti123@gmail.com>
2017-04-27 15:03:44 +0300

    lisätty on delete cascade

--- init/tyaajat.sql
+++ init/tyaajat.sql
@@ -1,5 +1,6 @@
 CREATE TABLE Tyaajat (
-  duunarID CHAR(11) PRIMARY KEY
+  duunarID CHAR(11) PRIMARY KEY,
   vapaana BOOLEAN,
   FOREIGN KEY (duunarID) REFERENCES Tyaukko(tyaukonsosID)
+  ON DELETE CASCADE,
 );
