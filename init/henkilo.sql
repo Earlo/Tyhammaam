e58737b8018eda32bfe9e80c0baf3fe12d26d059
Laaden12345 <erkkipentti123@gmail.com>
2017-05-03 15:03:19 +0300

    Update henkilo.sql

--- init/henkilo.sql
+++ init/henkilo.sql
@@ -2,6 +2,5 @@ CREATE TABLE Henkilo (
  nimi CHAR(64),
  puhelin CHAR(32),
  osoite CHAR(128),
- henkilonID CHR(11) NOT NULL REFERENCES Henkilo(sosID),
- PRIMARY KEY(henkilonID)
+ sosID CHAR(11) PRIMARY KEY
 );
