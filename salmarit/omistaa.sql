4818628d51e360ddd563977dde7e4c89e72a531a
Laaden12345 <erkkipentti123@gmail.com>
2017-05-03 14:05:22 +0300

    Update omistaa.sql

--- salmarit/omistaa.sql
+++ salmarit/omistaa.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Omistaa (
  autorekkari CHAR(7) NOT NULL,
  henkiloID CHAR(11) NOT NULL,
- FOREIGN KEY (autorekkari) REFERENCES (Auto(rekkari)),
- FOREIGN KEY (henkiloID) REFERENCES (Henkilo(sosID))
+ FOREIGN KEY autorekkari REFERENCES Auto(rekkari),
+ FOREIGN KEY henkiloID REFERENCES Henkilo(sosID)
 );
