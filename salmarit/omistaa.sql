0345cb0a95d027270eb2da8cf1c6a74ed5f0e6b0
Thor-Touch Henri <thorth1@niksula.hut.fi>
2017-05-03 14:06:02 +0300

    Deleted salmarit

--- salmarit/omistaa.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE Omistaa (
- autorekkari CHAR(7) NOT NULL,
- henkiloID CHAR(11) NOT NULL,
- FOREIGN KEY (autorekkari) REFERENCES (Auto(rekkari)),
- FOREIGN KEY (henkiloID) REFERENCES (Henkilo(sosID))
-);
