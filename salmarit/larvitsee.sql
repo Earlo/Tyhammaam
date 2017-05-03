b9245b87b33d6a24d59261c07ad7e59429d2c165
Laaden12345 <erkkipentti123@gmail.com>
2017-05-03 14:50:41 +0300

    Create larvitsee.sql

---
+++ salmarit/larvitsee.sql
@@ -0,0 +1,6 @@
+CRETE TABLE Larvitsee (
+  toimnimi CHAR(50) NOT NULL,
+  ltyyppi CHAR(20) NOT NULL,
+  FOREIGN KEY (toimnimi) REFERENCES Toimenpide(nimi),
+  FOREIGN KEY (ltyyppi) REFERENCES Laite(tyyppi)
+);
