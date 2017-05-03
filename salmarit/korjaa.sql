ac4af95581b59edb66dfe848476f24f61b118ac9
Laaden12345 <erkkipentti123@gmail.com>
2017-05-03 14:04:58 +0300

    Update korjaa.sql

--- salmarit/korjaa.sql
+++ salmarit/korjaa.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Korjaa (
    autonrekkari CHAR(7) NOT NULL,
    huoltoID INT NOT NULL,
-   FOREIGN KEY (huoltoID) REFERENCES (Huolto(HID)),
-   FOREIGN KEY (autonrekkari) REFERENCES (Auto(rekkari))
+   FOREIGN KEY huoltoID REFERENCES Huolto(HID),
+   FOREIGN KEY autonrekkari REFERENCES Auto(rekkari)
 );
