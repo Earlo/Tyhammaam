9f0518629f68c4bf16cbf78adead9ed6f15a5684
Pollari Visa <pollarv1@glass.niksula.hut.fi>
2017-05-09 19:16:06 +0300

    add index

--- initialD/indeksit.sql
+++ initialD/indeksit.sql
@@ -2,15 +2,22 @@
 
 CREATE INDEX sosiaalitunnus ON Henkilo(SosID);
 
-CREATE INDEX huolto_id ON Huolto(HID);
-
-
-/*Laskuja pyörittelevä ja lähettelevä laskubotti tekee paljon kutsuta tietokantaa, siksi ne kannattaa indeksoidä*/
-CREATE INDEX laskunumero ON Lasku(laskunro);
-
 /*Kaupisen autokorjaamon käsitellessä expotentiaalisesti kasvavaa määrää autoja, niiden indeksöinti auttaa tiedon käsittelemistä*/
 CREATE INDEX rekisterinumero ON Auto(rekkari);
 
 
 /*Varaosatyyppejä on paljon, se että Huoltaja saa ripestäti tietää onko korjaamolla tarjolla Z23-55B-Ruuveja vai pitääkö tilata parantaa palvelun tasoa*/
 CREATE INDEX varaosatyyppi ON Varaosa(tyyppi);
+
+/*laitetyyppejä on paljon, se että Huoltaja saa ripestäti tietää onko korjaamolla tarjolla sopivaa laitetta vai pitääkö odottaa vapautumista*/
+CREATE INDEX laitetyyppIndeksi ON Laitetyyppi(tyyppi);
+CREATE INDEX laiteIndeksi ON Laite(sarjanro);
+
+/*Toimepiteen indesointi nopeuttaa uusien pakettien luontia ja customointia*/
+CREATE INDEX toimenpiidee ON Toimenpide(nimi);
+CREATE INDEX paketti ON Huoltopaketti(nimi);
+
+
+/*Keep your customers close, and your employees closer*/
+CREATE INDEX ukkeli ON Tyaukko(sosID);
+
